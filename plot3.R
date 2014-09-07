60b2ae798ed602cbaaf6c79d5792f6f949592fa5
rcdinesh <rcdinesh@gmail.com>
2014-09-08 00:43:59 +0530

    Final

--- plot3.R
+++ plot3.R
@@ -1,5 +1,14 @@
-png(file="plot3.png")
+png(filename='plot3.png',
+    width = 480,
+    height = 480,
+    units = "px")
 with(hpc,plot(Date, Sub_metering_1,type="l",ylab = "Global Active Power (Kilowatts)"))
 with(hpc,lines (Date, Sub_metering_2  ,col="red"))
 with(hpc,lines (Date, Sub_metering_3  ,col="blue"))
+# Add the legend in the top right corner
+legend("topright",
+       lty=1,
+       lwd=1,
+       col=c("black","red","blue"),
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -4,6 +4,11 @@ with(hpc,plot(Date, Global_active_power,type="l",ylab = "Global Active Power (Ki
 with(hpc,plot(Date, Sub_metering_1  ,ylab = "Global Active Power (Kilowatts)",type="l"))
 with(hpc,lines (Date, Sub_metering_2  ,col="red"))
 with(hpc,lines (Date, Sub_metering_3  ,col="blue"))
+legend("topright",
+       lty=1,
+       lwd=1,
+       col=c("black","red","blue"),cex=0.5
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 with(hpc,plot(Date, Voltage,type="l",ylab = "Voltage"))
 with(hpc,plot(Date, Global_reactive_power,ylab = "Global Reactive Power",type="l"))
 dev.off()
